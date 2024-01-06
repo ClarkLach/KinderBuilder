eb0a02a91d4789766b5618f928d2b3af6ef26f08
ClarkLach <102780236+ClarkLach@users.noreply.github.com>
2024-01-06 00:45:40 -0500

    Removed Drag and Drop
    Makes much more sense and simplifies things to just click the words, likely would work better on touchscreen. Will need to add some dragging back to rearrange words within sentence.

--- script.js
+++ script.js
@@ -1,106 +1,64 @@
-function allowDrop(ev) {
-    ev.preventDefault();
-}
+const nouns = [
+    "acorn", "apple", "ball", "butterfly",
+    "cat", "cloud", "dolphin", "dog",
+    "elephant", "fish", "flower", "guitar",
+    "giraffe", "hat", "house", "ice cream",
+    "igloo", "jellyfish", "juice", "key",
+    "kite", "lamp", "lion", "moon",
+    "mountain", "nest", "notebook", "ocean",
+    "octopus", "penguin", "queen", "rainbow",
+    "sun", "turtle", "umbrella", "van",
+    "watermelon", "xylophone", "yo-yo", "zebra"
+];
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-    console.log('Element dragged:', ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    var draggedElement = document.getElementById(data);
-    var targetElement = ev.target;
-
-    // Disallow specific areas by id
-    if (ev.target.id === "search-box" || 
-        ev.target.id === "confirm-button" || 
-        ev.target.id === "search-results" || 
-        ev.target.id === "search-container" ||
-        ev.target.classList.contains('title')) {
-        return;
-    }
-
-    if (draggedElement.id === "period" && 
-        !targetElement.classList.contains('trashcan') &&
-        !targetElement.draggable) {
-
-        var copy = draggedElement.cloneNode(true);
-        copy.original = true;
-        targetElement.appendChild(copy);
-        return;
-    } else if (targetElement.classList.contains('trashcan') && draggedElement.original) {
-
-        draggedElement.remove();
-        return;
-    }
-
-    // Check if the target element is draggable
-    if (!targetElement.draggable && !targetElement.classList.contains('trashcan')) {
-        // Proceed with drop
-        targetElement.appendChild(draggedElement);
-        return;
-    }
-};
+const sight_words = [
+    "all", "am", "are", "at",
+    "ate", "be", "black", "brown",
+    "but", "came", "did", "do",
+    "eat", "four", "get", "good",
+    "have", "he", "into", "like",
+    "must", "new", "no", "now",
+    "on", "our", "out", "please",
+    "pretty", "ran", "ride", "saw",
+    "say", "she", "so", "soon",
+    "that", "there", "they", "this",
+    "too", "under", "want", "was",
+    "well", "went", "what", "white",
+    "who", "will", "with", "yes"
+];
 
 function clearSentenceBox() {
     const sentenceBox = document.getElementById('sentence-text');
     sentenceBox.innerHTML = '';
 };
 
-const nouns = [
-    "apple", "ball", "cat", "dog", "elephant",
-    "flower", "giraffe", "hat", "ice cream", "jellyfish",
-    "kite", "lion", "moon", "nest", "octopus",
-    "penguin", "queen", "rainbow", "sun", "turtle",
-    "umbrella", "van", "watermelon", "xylophone", "yo-yo",
-    "zebra", "acorn", "butterfly", "cloud", "dolphin",
-    "fish", "guitar", "house", "igloo", "juice",
-    "key", "lamp", "mountain", "notebook", "ocean"
-];
-
-const sight_words = [
-    "all", "out",
-    "am", "are",
-    "please", "pretty",
-    "at", "ran", "ate", "ride", "be", "saw",
-    "black", "say", "brown", "she",
-    "but", "so", "came", "soon",
-    "did", "that", "do", "there",
-    "eat", "they", "four", "this", "get", "too",
-    "good", "under",
-    "have", "want",
-    "he", "was", "into", "well",
-    "like", "went", "must", "what",
-    "new", "white", "no", "who",
-    "now", "will", "on", "with",
-    "our", "yes"
-];
-
 // Function to create initial word div elements
-function createDivs(category, dataList) {
+function createWordDivsFromArray(category, dataList) {
     const container = document.querySelector(category);
 
-    dataList.forEach((content, index) => { // Loop through arrays to create divs
+    dataList.forEach((content) => { // Loop through arrays to create divs
         const div = document.createElement('div');
-
-        div.id = `${category}${index+1}`;
         div.className = 'word';
-        div.draggable = true;
         div.textContent = content;
-        div.original = true;
-        div.setAttribute('ondragstart', 'drag(event)');
-        div.setAttribute('ondrag', 'onDrag(event)');
-        div.setAttribute('ondragend', 'onDragEnd(event)');
-
         container.appendChild(div);
-
     });
 }
 
-createDivs(".nouns", nouns);
-createDivs(".sight", sight_words);
+createWordDivsFromArray(".nouns", nouns);
+createWordDivsFromArray(".sight", sight_words);
+
+
+// Click action for word divs
+document.addEventListener('click', function (event) {
+    if (event.target.className === 'word' && !(event.target.parentElement.id === 'sentence-text')) {
+        const word = event.target.cloneNode(true);
+        const sentenceBox = document.getElementById('sentence-text');
+        sentenceBox.appendChild(word);
+    }
+    if (event.target.className === 'word' && event.target.parentElement.id === 'sentence-text') {
+        event.target.remove();
+    }
+});
 
 
 // Search List
@@ -112,8 +70,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     searchBox.addEventListener('input', function () {
         const searchTerm = searchBox.value.toLowerCase();
-        const filteredWords = sight_words.filter(word => word.toLowerCase().startsWith(searchTerm));
-
+        const all_words = nouns.concat(sight_words);
+        const filteredWords = all_words.filter(word => word.toLowerCase().startsWith(searchTerm));
         displayResults(filteredWords);
     });
 
@@ -125,50 +83,42 @@ document.addEventListener('DOMContentLoaded', function () {
                 const listItem = document.createElement('li');
                 listItem.textContent = result;
                 listItem.addEventListener('click', function () {
-                    searchBox.value = result;
                     searchResults.style.display = 'none';
+                    createDiv(result);
                 });
                 searchResults.appendChild(listItem);
             });
 
+            // Used to limit height of search results proportionally.
             const grandparentDivHeight = searchResults.parentElement.parentElement.clientHeight;
             searchResults.style.maxHeight = (grandparentDivHeight * 0.35) + 'px';
             searchResults.style.overflowY = 'auto';
-
             searchResults.style.display = 'block';
+
         } else {
             searchResults.style.display = 'none';
         }
     }
 
-    confirmButton.addEventListener('click', createDiv); // Click button to create div
-    searchBox.addEventListener('keydown', function (event) { // Enter to create div
+    confirmButton.addEventListener('click', function() { // Click button to create div
+        createDiv(searchBox.value); 
+    });
+
+    searchBox.addEventListener('keydown', function (event) { // Press enter to create div
         if (event.key === 'Enter') {
-            createDiv();
+            createDiv(searchBox.value);
         }
     });
 
-
-    function createDiv() {
-        const content = searchBox.value;
+    function createDiv(word) {
+        const content = word;
         if (content.trim() !== '') {
             const div = document.createElement('div');
-            const uniqueID = generateUniqueID();
-
-            div.id = uniqueID;
             div.className = 'word';
-            div.draggable = true;
             div.textContent = content;
-            div.original = true;
-            div.setAttribute('ondragstart', 'drag(event)');
-
             outputContainer.appendChild(div);
             searchBox.value = ''; // Clear the search box after creating the div
         }
-    }
-
-    function generateUniqueID() {
-        return 'word-' + Date.now();
     }
 
     document.addEventListener('click', function (event) {
@@ -186,11 +136,10 @@ function alertTest() {
 
     for (let i = 0; i < sentence_text.children.length; i++) {
         // Access each child element
-        sentenceArr.push(sentence_text.children[i].innerText);
-    
-    }
+        sentenceArr.push(sentence_text.children[i].innerText); 
+    };
 
     if (sentenceArr.length > 0) {
         alert(sentenceArr.join(' '));
-    }
+    };
 };
